a6166f6b05f68e7a72115313b5954e901d520854
mmupfumir <mmupfumir@hartford.edu>
2024-11-06 17:02:45 -0500

    all changes

--- src/app/components/works.tsx
+++ src/app/components/works.tsx
@@ -24,7 +24,7 @@ const worksData = [
 export default function Works() {
   return (
     <section className="px-5">
-      <div className="flex overflow-hidden bg-[#f9f9f9] sm:py-5 md:py-5 rounded-3xl">
+      <div className="flex overflow-hidden bg-[#f9f9f9] ring-1 ring-black/10 sm:py-5 md:py-5 rounded-3xl">
         <motion.div
           initial={{ x: "0" }}
           animate={{ x: "-50%" }}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <div>
     <Navbar />
     <main className="pt-3">
-      <h1 className=' px-5 pt-40 sm:text-8xl md:text-[12.5rem] leading-none text-[#eaeaea] font-normal tracking-tighter'>image and interface</h1>
-      <h1 className='px-5 pb-5 sm:text-8xl md:text-[12.5rem] leading-none text-[#eaeaea] font-normal tracking-tighter text-right'>designer</h1>
+      <h1 className=' px-5 pt-40 sm:text-8xl md:text-[12.5rem] leading-none text-[#eaeaea] font-normal tracking-tighter'>image & interface</h1>
+      <h1 className='px-5 pb-5 sm:text-8xl md:text-[12.5rem] leading-none text-[#eaeaea] font-normal tracking-tighter text-right'>designer & engineer</h1>
       <Works />
       <div className="px-5 flex md:flex-row justify-between items-end">
       </div>
