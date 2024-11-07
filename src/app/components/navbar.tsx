019384ef291253d4baef055162588a22f1da6538
mmupfumir <mmupfumir@hartford.edu>
2024-11-07 15:30:02 -0500

    fixes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
 
     <div className="bg-white  px-5 py-5 tracking-tight flex flex-row items-center justify-between gap-1 font-light">
-      <h1 className="p-2 px-3 ring-1 ring-black rounded-full text-black text-xs md:text-sm transition duration-500">Milton</h1>
+      <h1 className="p-2 px-3 ring-1 ring-black rounded-full text-black text-xs md:text-sm transition duration-500">Milton Mupfumira</h1>
       <div className="flex flex-row gap-3 items-center text-xs md:text-sm">
         <Link href="/Projects" className="bg-[#f9f9f9] p-2 px-3 rounded-full text-black transition duration-500">Projects</Link>
         <Link href="mailto:mmupfumir@hartford.edu" className="bg-black p-2 px-3 rounded-full text-white  rounded-fulltransition duration-500 font-normal">Contact</Link>
